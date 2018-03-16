309c6265e02ef47eb061d94a8b61205e6b820944
Tao Wen <taowen@gmail.com>
2018-03-16 22:59:44 +0800

    add context

---
+++ context.go
@@ -0,0 +1,127 @@
+package countlog
+
+import (
+	"unsafe"
+	"context"
+	"github.com/modern-go/countlog/logger"
+	"github.com/modern-go/reflect2"
+)
+
+func Ctx(ctx context.Context) *Context {
+	wrapped, isWrapped := ctx.(*Context)
+	if isWrapped {
+		return wrapped
+	}
+	return &Context{Context: ctx, logContext: &logger.LogContext{}}
+}
+
+type Context struct {
+	context.Context
+	logContext *logger.LogContext
+}
+
+func (ctx *Context) Value(key interface{}) interface{} {
+	if ctx == nil {
+		return nil
+	}
+	if key == logger.LogContextKey {
+		return ctx.logContext
+	}
+	return ctx.Context.Value(key)
+}
+
+func (ctx *Context) Trace(event string, properties ...interface{}) {
+	if LevelTrace < logger.MinLevel {
+		return
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelTrace, event, "", ctx, nil, *(*[]interface{})(ptr))
+}
+
+func (ctx *Context) TraceCall(event string, err error, properties ...interface{}) error {
+	if err != nil {
+		ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+		return log(LevelWarn, event, "call", ctx, err, *(*[]interface{})(ptr))
+	}
+	if LevelTrace < logger.MinLevel {
+		return nil
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelTrace, event, "call", ctx, err, *(*[]interface{})(ptr))
+	return nil
+}
+
+func (ctx *Context) Debug(event string, properties ...interface{}) {
+	if LevelDebug < logger.MinLevel {
+		return
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelDebug, event, "", ctx, nil, *(*[]interface{})(ptr))
+}
+
+func (ctx *Context) DebugCall(event string, err error, properties ...interface{}) error {
+	if err != nil {
+		ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+		return log(LevelWarn, event, "call", ctx, err, *(*[]interface{})(ptr))
+	}
+	if LevelDebug < logger.MinLevel {
+		return nil
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelDebug, event, "call", ctx, err, *(*[]interface{})(ptr))
+	return nil
+}
+
+func (ctx *Context) Info(event string, properties ...interface{}) {
+	if LevelInfo < logger.MinLevel {
+		return
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelInfo, event, "", ctx, nil, *(*[]interface{})(ptr))
+}
+
+func (ctx *Context) InfoCall(event string, err error, properties ...interface{}) error {
+	if err != nil {
+		ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+		return log(LevelWarn, event, "call", ctx, err, *(*[]interface{})(ptr))
+	}
+	if LevelInfo < logger.MinLevel {
+		return nil
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelInfo, event, "call", ctx, err, *(*[]interface{})(ptr))
+	return nil
+}
+
+func (ctx *Context) LogAccess(event string, err error, properties ...interface{}) error {
+	if err != nil {
+		ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+		return log(LevelError, event, "call", ctx, err, *(*[]interface{})(ptr))
+	}
+	if LevelInfo < logger.MinLevel {
+		return nil
+	}
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelInfo, event, "call", ctx, err, *(*[]interface{})(ptr))
+	return nil
+}
+
+func (ctx *Context) Warn(event string, properties ...interface{}) {
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelWarn, event, "", ctx, nil, *(*[]interface{})(ptr))
+}
+
+func (ctx *Context) Error(event string, properties ...interface{}) {
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelError, event, "", ctx, nil, *(*[]interface{})(ptr))
+}
+
+func (ctx *Context) Fatal(event string, properties ...interface{}) {
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
+	log(LevelFatal, event, "", ctx, nil, *(*[]interface{})(ptr))
+}
+
+func (ctx *Context) Add(key string, value interface{}) {
+	ctx.logContext.Properties = append(ctx.logContext.Properties, key)
+	ctx.logContext.Properties = append(ctx.logContext.Properties, value)
+}
--- countlog.go
+++ countlog.go
@@ -53,7 +53,7 @@ func Debug(event string, properties ...interface{}) {
 	if LevelDebug < logger.MinLevel {
 		return
 	}
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelDebug, event, "", nil, nil, *(*[]interface{})(ptr))
 }
 
@@ -61,13 +61,13 @@ func Debug(event string, properties ...interface{}) {
 // DebugCall will output individual log entries in DEBUG_CALL level (TRACE includes DEBUG_CALL)
 func DebugCall(event string, err error, properties ...interface{}) error {
 	if err != nil {
-		ptr := unsafe.Pointer(&properties)
+		ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 		return log(LevelWarn, event, "call", nil, err, *(*[]interface{})(ptr))
 	}
 	if LevelDebug < logger.MinLevel {
 		return nil
 	}
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelDebug, event, "call", nil, err, *(*[]interface{})(ptr))
 	return nil
 }
@@ -76,7 +76,7 @@ func Info(event string, properties ...interface{}) {
 	if LevelInfo < logger.MinLevel {
 		return
 	}
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelInfo, event, "", nil, nil, *(*[]interface{})(ptr))
 }
 
@@ -84,29 +84,29 @@ func Info(event string, properties ...interface{}) {
 // InfoCall will output individual log entries in INFO_CALL level (DEBUG includes INFO_CALL)
 func InfoCall(event string, err error, properties ...interface{}) error {
 	if err != nil {
-		ptr := unsafe.Pointer(&properties)
+		ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 		return log(LevelWarn, event, "call", nil, err, *(*[]interface{})(ptr))
 	}
 	if LevelInfo < logger.MinLevel {
 		return nil
 	}
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelInfo, event, "call", nil, err, *(*[]interface{})(ptr))
 	return nil
 }
 
 func Warn(event string, properties ...interface{}) {
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelWarn, event, "", nil, nil, *(*[]interface{})(ptr))
 }
 
 func Error(event string, properties ...interface{}) {
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelError, event, "", nil, nil, *(*[]interface{})(ptr))
 }
 
 func Fatal(event string, properties ...interface{}) {
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(LevelFatal, event, "", nil, nil, *(*[]interface{})(ptr))
 }
 
@@ -114,7 +114,7 @@ func Log(level int, event string, properties ...interface{}) {
 	if level < logger.MinLevel {
 		return
 	}
-	ptr := unsafe.Pointer(&properties)
+	ptr := reflect2.NoEscape(unsafe.Pointer(&properties))
 	log(level, event, "", nil, nil, *(*[]interface{})(ptr))
 }
 
